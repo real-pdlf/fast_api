9b86440d3fac2fcdb90524abbfefe2279f8191ae
Royal Pandalfuul <ga7w7arcanjo@gmail.com>
2023-07-25 21:14:11 -0300

    added pydentic[email] and SQLAlchemy to pyproject.toml

---
+++ fast_api/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(DeclarativeBase):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    password: Mapped[str]
+    email: Mapped[str]
+
+    
