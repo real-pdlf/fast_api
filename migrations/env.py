abeb381c3530ac1631137792ef87e9e8a9c93e5c
Royal Pandalfuul <ga7w7arcanjo@gmail.com>
2023-09-19 21:56:16 -0300

    fix: database_url

--- migrations/env.py
+++ migrations/env.py
@@ -9,7 +9,7 @@ from fast_api.settings import Settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', Settings.DATABASE_URL)
+config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
