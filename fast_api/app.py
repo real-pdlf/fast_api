bd84f49d376e63c661ac26e7e4ecb3e3a5fd7c1b
RoyalPandalfuul <61811141+RoyalPandalfuul@users.noreply.github.com>
2023-09-20 13:50:24 -0300

    merge dev on main (#1)
    * fix: poetry.lock
    
    * add: readme.md
    
    * lint: app.py and add alembic
    
    * Delete .coverage
    
    * add database.db to .gitignore
    
    * fix: database_url
    
    * add alembic revision

--- fast_api/app.py
+++ fast_api/app.py
@@ -4,22 +4,29 @@ from fast_api.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
-database = []
+database = [] #only for tests
 
 
-@app.get('/users/', response_model=UserList)
-# the function 'read_root' stopped working
-# after 'read_users' been implemented, why?
-# def read_root():
-#     return {'message': 'salve! TwT'}
+@app.get('/')
+def read_root():
+    """returns a hello world message"""
+    return {'message': 'Hello World! Im a root!'}
 
 
+@app.get('/users/')
 def read_users():
+    """view all users registered in database"""
     return {'users': database}
+
+
+@app.get('/users/{user_id}')
+def read_user(user_id: int):
+    return
 
 
 @app.post('/users/', status_code=201, response_model=UserPublic)
 def create_user(user: UserSchema):
+    """add one new user to database"""
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
 
     database.append(user_with_id)
@@ -29,6 +36,7 @@ def create_user(user: UserSchema):
 
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
+    """update informations about one user, with his id"""
     if user_id > len(database) or user_id < 1:
         raise HTTPException(status_code=404, detail='User not found')
 
@@ -40,6 +48,7 @@ def update_user(user_id: int, user: UserSchema):
 
 @app.delete('/users/{user_id}', response_model=Message)
 def delete_user(user_id: int):
+    """delete one user from the database, using his id"""
     if user_id > len(database) or user_id < 1:
         raise HTTPException(status_code=404, detail='User not found')
     del database[user_id - 1]
