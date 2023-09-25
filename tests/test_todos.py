d95a7a41df52040958c20f05f56a21796b8a1a96
Royal Pandalfuul <ga7w7arcanjo@gmail.com>
2023-09-25 19:44:31 -0300

    fix: decription field name

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -4,14 +4,15 @@ def test_create_todo(client, token):
         headers={'Authorization': f'Bearer {token}'},
         json={
             'title': 'Test todo',
-            'desciption': 'Test todo description',
+            'description': 'Test todo description',
             'state': 'draft',
         },
     )
+    print(response.json())
     assert response.json() == {
         'id': 1,
         'title': 'Test todo',
-        'desciption': 'Test todo description',
+        'description': 'Test todo description',
         'state': 'draft',
     }
-    assert response.status_code == 201
+    assert response.status_code == 200
